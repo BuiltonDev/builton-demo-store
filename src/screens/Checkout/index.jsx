e280d4f5238c892dfb620a3191ad746745a422e6
Tigran <tigrangit@gmail.com>
2019-07-18 17:03:42 +0300

    progress with some animation timings

--- src/screens/Checkout/index.jsx
+++ src/screens/Checkout/index.jsx
@@ -22,12 +22,33 @@ const Checkout = () => {
     if (checkout) {
       const stepsVals = [...Object.values(checkout)];
       if (match.params.step) {
+        let length = 0;
         for (let i = 0; i < stepsVals.length; i += 1) {
           if (match.params.step === stepsVals[i].title) {
             setStep(i);
+            length = i;
             break;
           }
         }
+        const elements = document.getElementsByClassName("checkout-steps-container");
+        if (elements && elements[0] && elements[0].children) {
+          const childrenElements = elements[0].children;
+          let defaultTransitionDelay = 0;
+          for (let i = 0; i < childrenElements.length; i += 1) {
+            if (i < length + 1) {
+              defaultTransitionDelay += 350;
+              for (let x = 0; x < childrenElements[i].children.length; x +=1) {
+                if (childrenElements[i].children[x].className.includes('checkout-step-progress')) {
+                  childrenElements[i].children[x].children[0].style.transitionDelay = `${defaultTransitionDelay}ms`;
+                } else if (childrenElements[i].children[x].className.includes('checkout-step-circle')) {
+                  childrenElements[i].children[x].children[0].style.animationDelay = `${defaultTransitionDelay + 350}ms`;
+                }
+              }
+
+            }
+          }
+        }
+
       } else {
         for (let i = 0; i < stepsVals.length; i += 1) {
           if (!stepsVals[i].complete) {
@@ -63,7 +84,7 @@ const Checkout = () => {
 
     history.push(`/checkout/${checkout[typeof stepNumb !== 'undefined' ? stepNumb : step + 1].title}`);
   };
-  
+
   return (
     <div className="main-container">
       <Header />
@@ -81,28 +102,36 @@ const Checkout = () => {
                 <div className="checkout-step-title">Your bag</div>
               </div>
               <div className={`checkout-step ${step >= 1 ? 'active-step' : ''}`}>
-                <div className="checkout-step-progress"/>
+                <div className="checkout-step-progress">
+                  <div/>
+                </div>
                 <div className="checkout-step-circle" onClick={() => step >= 1 && pushStep(1)}>
                   <div/>
                 </div>
                 <div className="checkout-step-title">Authentication</div>
               </div>
               <div className={`checkout-step ${step >= 2 ? 'active-step' : ''}`}>
-                <div className="checkout-step-progress"/>
+                <div className="checkout-step-progress">
+                  <div/>
+                </div>
                 <div className="checkout-step-circle" onClick={() => step >= 2 && pushStep(2)}>
                   <div/>
                 </div>
                 <div className="checkout-step-title">Payment method</div>
               </div>
               <div className={`checkout-step ${step >= 3 ? 'active-step' : ''}`}>
-                <div className="checkout-step-progress"/>
+                <div className="checkout-step-progress">
+                  <div/>
+                </div>
                 <div className="checkout-step-circle" onClick={() => step >= 3 && pushStep(3)}>
                   <div/>
                 </div>
                 <div className="checkout-step-title">Delivery address</div>
               </div>
               <div className={`checkout-step ${step >= 4 ? 'active-step' : ''}`}>
-                <div className="checkout-step-progress"/>
+                <div className="checkout-step-progress">
+                  <div/>
+                </div>
                 <div className="checkout-step-circle" onClick={() => step >= 4 && pushStep(4)}>
                   <div/>
                 </div>

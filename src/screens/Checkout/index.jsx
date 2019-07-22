688c4c0efd1bee0301556146a752f1ea530adcd8
Tigran <tigrangit@gmail.com>
2019-07-22 10:28:50 +0300

    export step one

--- src/screens/Checkout/index.jsx
+++ src/screens/Checkout/index.jsx
@@ -5,80 +5,13 @@ import { useGlobal, useDispatch } from 'reactn';
 import './index.scss';
 import useReactRouter from "use-react-router";
 import Header from "../../components/Header";
-import Button from "../../components/Button";
 import CheckoutNavigation from "../../components/CheckoutNavigation";
-import {getProductName, getSneakersSize} from "../../utils/productModifiers";
-import RemoveShopping from "../../assets/icons/remove_shopping";
+import StepOne from "./stepOne";
 
 
 
 const Checkout = () => {
-  const [bag] = useGlobal('bag');
   const [step, setStep] = useState(null);
-  const removeItemFrombag = useDispatch("removeItemFromBag");
-  const { history } = useReactRouter();
-
-  const removeItem = itemId => {
-    removeItemFrombag(itemId);
-  };
-
-  console.log(bag);
-
-  const calculateTotalAmount = () => {
-    let total = 0;
-    for (let i = 0; i < bag.length; i += 1) {
-      total += bag[i].product.price
-    }
-    return total;
-  };
-
-  const renderStep1 = () => {
-    if (!bag || !bag.length) return null;
-    return (
-      <>
-        <div className="checkout-items-container-title">
-          Your items
-        </div>
-        <div className="checkout-items-header">
-          <div className="checkout-name-col">Brand</div>
-          <div className="checkout-description-col">Model</div>
-          <div className="checkout-size-col">Size</div>
-          <div className="checkout-price-col">Price</div>
-          <div className="checkout-remove-col"/>
-        </div>
-        {bag && bag.map((item, index) => (
-          <div className="checkout-bag-item-row" key={`bag-item-${item.product.id}-${index}`} onClick={() => history.push(`/product_list/${item.category}/${item.product._id.$oid}`)}>
-            <div className="checkout-name-col">
-              {getProductName(item.product.name)}
-            </div>
-            <div className="checkout-description-col">
-              {item.product.short_description}
-            </div>
-            <div className="checkout-size-col">
-              {getSneakersSize(item.size)}
-            </div>
-            <div className="checkout-price-col">
-              {item.product.price} {item.product.currency}
-            </div>
-            <div className="checkout-remove-col">
-              <div
-                className="remove-bag-item"
-                onClick={() => removeItem(item.size._id.$oid)}
-              >
-                <RemoveShopping color="#c5c5c5" />
-              </div>
-            </div>
-          </div>
-          ))}
-          <div className="checkout-bag-total-row">
-            <div className="checkout-bag-total-title">Total</div>
-            <div className="checkout-bag-total">{calculateTotalAmount()} {bag[0].product.currency}</div>
-            <div className="checkout-bag-empty" />
-          </div>
-      </>
-    )
-  };
-
   return (
     <div className="main-container">
       <Header />
@@ -86,7 +19,7 @@ const Checkout = () => {
         <div className="checkout-container">
           <div className="checkout-inner-container">
             <div className={`checkout-items-container ${step === 0 ? 'show-container' : 'hide-container'}`} >
-              {renderStep1()}
+              <StepOne/>
             </div>
             <div className={`checkout-items-container ${step === 1 ? 'show-container' : 'hide-container'}`} >
               test2
---
+++ src/screens/Checkout/stepOne/index.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  getProductName,
+  getSneakersSize
+} from "../../../utils/productModifiers";
+import RemoveShopping from "../../../assets/icons/remove_shopping";
+import useReactRouter from "use-react-router";
+import { useDispatch, useGlobal } from "reactn";
+
+const StepOne = () => {
+  const [bag] = useGlobal("bag");
+
+  const removeItemFrombag = useDispatch("removeItemFromBag");
+  const { history } = useReactRouter();
+
+  const removeItem = (e, itemId) => {
+    e.stopPropagation();
+    removeItemFrombag(itemId);
+  };
+
+  const calculateTotalAmount = () => {
+    let total = 0;
+    for (let i = 0; i < bag.length; i += 1) {
+      total += bag[i].product.price;
+    }
+    return total;
+  };
+
+  const renderBagRow = (item, index) => {
+    return (
+      <div
+        className="checkout-bag-item-row"
+        key={`bag-item-${item.product.id}-${index}`}
+        onClick={() =>
+          history.push(
+            `/product_list/${item.category}/${item.product._id.$oid}`
+          )
+        }
+      >
+        <div className="checkout-name-col">
+          {getProductName(item.product.name)}
+        </div>
+        <div className="checkout-description-col">
+          {item.product.short_description}
+        </div>
+        <div className="checkout-size-col">{getSneakersSize(item.size)}</div>
+        <div className="checkout-price-col">
+          {item.product.price} {item.product.currency}
+        </div>
+        <div className="checkout-remove-col">
+          <div
+            className="remove-bag-item"
+            onClick={e => removeItem(e, item.size._id.$oid)}
+          >
+            <RemoveShopping color="#c5c5c5" />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  if (!bag && !bag.length) return null;
+
+  return (
+    <>
+      <div className="checkout-items-container-title">Your items</div>
+      <div className="checkout-items-header">
+        <div className="checkout-name-col">Brand</div>
+        <div className="checkout-description-col">Model</div>
+        <div className="checkout-size-col">Size</div>
+        <div className="checkout-price-col">Price</div>
+        <div className="checkout-remove-col" />
+      </div>
+      {bag && bag.map((item, index) => renderBagRow(item, index))}
+      <div className="checkout-bag-total-row">
+        <div className="checkout-bag-total-title">Total</div>
+        <div className="checkout-bag-total">
+          {calculateTotalAmount()} {bag[0].product.currency}
+        </div>
+        <div className="checkout-bag-empty" />
+      </div>
+    </>
+  );
+};
+
+export default StepOne;

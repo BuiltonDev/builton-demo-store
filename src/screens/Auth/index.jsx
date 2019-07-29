ad486ea57f76e63cd2eb499d02574372e0a2ed5c
Tigran <tigrangit@gmail.com>
2019-07-29 15:49:21 +0300

    fix stuff

--- src/screens/Auth/index.jsx
+++ src/screens/Auth/index.jsx
@@ -3,11 +3,8 @@ import { withRouter } from "react-router-dom";
 import "./index.scss";
 import BuiltonLogo from "../../components/BuiltonLogo";
 import AuthForm from "./authForm";
-import useReactRouter from "use-react-router";
 
 const Auth = () => {
-  const { history } = useReactRouter();
-
   return (
     <div className="wrapper">
       <BuiltonLogo style={{ position: "absolute", top: 12, left: 48 }} />

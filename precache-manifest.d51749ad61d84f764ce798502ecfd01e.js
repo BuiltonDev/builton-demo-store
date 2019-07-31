8c1f65aea965b6de4c55cd9b7488f8aca677284a
Tigran <tigrangit@gmail.com>
2019-07-31 14:49:01 +0300

    Updates

---
+++ precache-manifest.d51749ad61d84f764ce798502ecfd01e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d148900b3ec4b222fcde4270d281884d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a6fdbc87313dfbd6a4e",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "526b5433c01afd1ae700",
+    "url": "/static/css/main.fe4a2925.chunk.css"
+  },
+  {
+    "revision": "6a6fdbc87313dfbd6a4e",
+    "url": "/static/js/2.3f56e4e9.chunk.js"
+  },
+  {
+    "revision": "526b5433c01afd1ae700",
+    "url": "/static/js/main.1cd742ff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6e2ef894ef8e63a8a3e8c35a6e6144a",
+    "url": "/static/media/adidas-logo.b6e2ef89.png"
+  },
+  {
+    "revision": "513b7268be76f083c4abdeb0f73b4815",
+    "url": "/static/media/background.513b7268.png"
+  },
+  {
+    "revision": "aadac220889e1f65feb3af10f0f812a7",
+    "url": "/static/media/nike-logo.aadac220.png"
+  }
+]);
